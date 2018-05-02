6e78d4b5f4c387da6bf89cf19539ba02263290c1
Deisun <rdouma81@gmail.com>
2018-05-01 23:41:34 -0400

    Implenting index and final templates

--- measurements/admin.py
+++ measurements/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from .models import Area, Category, Location
 
+class LocationInLine(admin.TabularInline):
+    model = Location
+
 
 class AreaAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields' : ['name'] } ),
+        (None, {'fields' : ['id']}),
+        (None, {'fields' : ['name']}),
         (None, {'fields' : ['longitude']}),
         (None, {'fields' : ['latitude']}),
         ]
+
+    inlines = [LocationInLine]
 
 
 class CategoryAdmin(admin.ModelAdmin):
--- measurements/views.py
+++ measurements/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Area
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    area_list = Area.objects.all()
+    context = { 'area_list' : area_list, }
+    return render(request, 'measurements/index.html',context)
+
+
+
+def final(request):
+    return HttpResponse("this is the final page")
